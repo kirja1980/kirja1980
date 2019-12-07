537fd6ae24cb4608556f02deb5cbdcd1af379032
krotoner <42904622+krotoner@users.noreply.github.com>
2019-12-07 16:35:44 +0300

    Update errors.c

---
+++ Kostya/errors.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void errors(int name_error){
+    switch{
+        case 1{
+            printf("ФУНКЦИЯ ВВОД")
+        }
+        case 2{
+            printf("ФУНКЦИЯ ВЫВОД")
+        }
+    }
+}

8f218d30a6594931d439106a70cb96ce8642408c
Sergey Mazin <55027867+imserega@users.noreply.github.com>
2019-12-07 22:47:26 +1000

    Delete sum_before_and_after_negative.h

--- sum_before_and_after_negative.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "index_last_negative.h"
-#include "sum_between_negative.h"
-#include "sum_before_and_after_negative.h"
-
-int sum_before_and_after_negative(int array[], int count);
